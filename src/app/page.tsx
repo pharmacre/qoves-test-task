4f70641bd21f9450c97bc1917d8a331357911563
vmch <85562802+pharmacre@users.noreply.github.com>
2026-09-12 14:11:55 +0300

    refactor: spacing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main>
       <PersonalizedAnalysis />
-      <Spacing desktopOnly />
+      <Spacing />
       <PersonalizedAesthetics />
       <Spacing />
     </main>
--- src/components/spacing/Spacing.tsx
+++ src/components/spacing/Spacing.tsx
@@ -2,16 +2,12 @@ import { GridRow } from "@/components/grid-row/GridRow";
 
 import styles from "./Spacing.module.scss";
 
-type SpacingProps = Readonly<{
-  desktopOnly?: boolean;
-}>;
-
-export function Spacing({ desktopOnly = false }: SpacingProps) {
-  const spacing = (
-    <GridRow topLine bottomLine>
-      <div className={styles.box} />
-    </GridRow>
+export function Spacing() {
+  return (
+    <div className={styles.desktopOnly}>
+      <GridRow topLine bottomLine>
+        <div className={styles.box} />
+      </GridRow>
+    </div>
   );
-
-  return desktopOnly ? <div className={styles.desktopOnly}>{spacing}</div> : spacing;
 }
